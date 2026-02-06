67a3f7e9075932c0feda1d308b2a8a4ea3dccf60
Pedro Macedo-oss <pedro.h.macedo8@aluno.senai.br>
2026-02-06 09:50:57 -0300

    arquivo saida

---
+++ fundamentos/exemplos/saida.js
@@ -0,0 +1,5 @@
+const nome = prompt('qual seu nome')
+const endereco = prompt('qual seu endereçp')
+const idade = prompt('qual sua idade')
+
+alert(`${nome}, voce tem ${idade} anos,\n e mora no endereço ${endereco}`)
