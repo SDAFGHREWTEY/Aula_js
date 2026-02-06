5d898d57df223ca012175552842331ab78730323
Pedro Macedo-oss <pedro.h.macedo8@aluno.senai.br>
2026-02-06 09:22:33 -0300

    arquivo informaçao

---
+++ fundamentos/exemplos/informacao.js
@@ -0,0 +1,12 @@
+const curso =  ' Tecnico em desenvolvimento de Sistemas'
+const ano = 2025
+const N_M_aprovar = 50
+const N_M_reprovado = 49.9
+const C_tecnico = true
+
+console.log('informaçoes academicas')
+console.log('curso:' ,curso)
+console.log(`ano: ${ano}`)
+console.log(`Nota pra aprovar ${N_M_aprovar}`)
+console.log(`Nota pra reprovar ${N_M_reprovado}`)
+console.log(`e curso tecnico: ${C_tecnico}`)
